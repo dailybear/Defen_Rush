53965c78a035f075af17cb3b6b8b59398b778fd6
bohun <slawkd2002@gmail.com>
2023-10-11 13:56:28 +0900

    Tile Coordinates

---
+++ Assets/Tile/B_TileCoordinate.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using TreeEditor;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[ExecuteAlways]
+public class B_TileCoordinate : MonoBehaviour
+{
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+
+    private void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+    }
+    private void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            DisplayCoordinates();
+            //nameParent();
+        }
+    }
+    void DisplayCoordinates()
+    {
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+        label.text = coordinates.x + "," + coordinates.y;
+    }
+    void nameParent()
+    {
+        transform.parent.name = coordinates.ToString();
+    }
+}
